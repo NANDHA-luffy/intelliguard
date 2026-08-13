a3c2a802bd4fe53a6d9bc590cd7e4ca324509d8e
Nandha <nandhamuthuabisheks.aids2023@citchennai.net@gmail.com>
2026-08-13 14:24:50 +0530

    Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from app.db.database import engine, Base
@@ -13,6 +14,13 @@ app.include_router(teams.router)
 app.include_router(agents.router)
 app.include_router(sessions.router)
 app.include_router(chat.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
